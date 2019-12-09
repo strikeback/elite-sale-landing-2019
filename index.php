b0571666a89a4b63205c4a6293a4318f5705c8d2
anton <anton@ms-net.ru>
2019-12-09 16:46:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+global $APPLICATION;
+$APPLICATION->SetPageProperty("keywords", "");
+$APPLICATION->SetPageProperty("description", "");
+$APPLICATION->SetTitle("");
+
+?>
+
+
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+
+?>
---
+++ urlrewrite.php
@@ -0,0 +1,10 @@
+<?
+$arUrlRewrite = array(
+    array(
+        "CONDITION" => "/kvartira-id-(.*)-v-.*/",
+        "RULE" => "CRM_ID=$1",
+        "ID" => "",
+        "PATH" => "/index.php",
+    ),
+);
+?>
